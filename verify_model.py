8db0880ec5e693ef9de93cae871364aead6adef2
SNMeans <suminmeans@gmail.com>
2024-11-24 23:17:40 -0500

    Created temporary script to verify that the best model was saved and able to be ran

---
+++ verify_model.py
@@ -0,0 +1,14 @@
+# 11/24/24 Script to verify that the best model was saved 
+
+from tensorflow.keras.models import load_model
+
+# Path to the saved model
+model_path = 'CNN-model/models/resnet50_best_model.h5'
+
+# Verify loading the model
+try:
+    model = load_model(model_path)
+    print("Model loaded successfully!")
+    print(model.summary())
+except Exception as e:
+    print(f"Error loading model: {e}")
