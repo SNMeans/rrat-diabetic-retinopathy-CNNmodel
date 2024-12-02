bdf20afb55a81ac684629bd6470110a05b18f799
James Neff <james.m.neff@gmail.com>
2024-12-02 17:10:30 -0500

    Organize application into router and enpoint files

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 from fastapi import FastAPI
+from api.router import router
 
-import random
-
-# Initialize FastAPI app
 app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"result": "Hello World"}
-
-
-@app.post("/analyze")
-def analyze_image():   
-    random_int = random.randint(0, 4)  # Generate a random integer between 0 and 4 as a placeholder
-    return {"result": random_int}
+app.include_router(router)
