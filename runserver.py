08fee585b70d007c4982b96e2dc6f3a16f544cf5
James Neff <james.m.neff@gmail.com>
2024-12-02 17:12:49 -0500

    format runserver.py

--- runserver.py
+++ runserver.py
@@ -6,7 +6,10 @@ import os
 load_dotenv()
 
 if __name__ == "__main__":
-    host = os.getenv("HOST", "127.0.0.1")  # Default to localhost if not set
-    port = int(os.getenv("PORT", 8888))    # Default to 8000 if not set
+    # Default to localhost if not set
+    host = os.getenv("HOST", "127.0.0.1")  
+    
+    # Default to 8888 if not set
+    port = int(os.getenv("PORT", 8888))    
     
     uvicorn.run("main:app", host=host, port=port, reload=True)
