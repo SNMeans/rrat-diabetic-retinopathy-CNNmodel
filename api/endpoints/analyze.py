120c7012152082ea2e52b7b67b11e0e0b6971bd0
James Neff <james.m.neff@gmail.com>
2024-12-02 17:10:30 -0500

    Organize application into router and enpoint files

---
+++ api/endpoints/analyze.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+import random
+
+router = APIRouter()
+
+
+@router.post("/")
+def analyze_image():   
+    
+    random_int = random.randint(0, 4)  # Generate a random integer between 0 and 4 as a placeholder
+    return {"result": random_int}
