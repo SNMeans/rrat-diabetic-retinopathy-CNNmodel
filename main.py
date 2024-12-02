3d8e19c05496e8b3f80795fc7045e9fe1d5b386f
James Neff <james.m.neff@gmail.com>
2024-12-02 16:27:40 -0500

    setup basic api server

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-# main.py
+from fastapi import FastAPI
 
+import random
+
+# Initialize FastAPI app
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"result": "Hello World"}
+
+
+@app.post("/analyze")
+def analyze_image():   
+    random_int = random.randint(0, 4)  # Generate a random integer between 0 and 4 as a placeholder
+    return {"result": random_int}
